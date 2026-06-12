9936c312be18d9c10f4050e9889d51035386e617
yadira <yadiraco304@gmail.com>
2026-06-12 16:59:08 -0500

    improving code

--- back-front/taskapi/src/app.module.ts
+++ back-front/taskapi/src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
-import { TasksModule } from './tasks/tasks.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from "@nestjs/common";
+import { TasksModule } from "./tasks/tasks.module";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), TasksModule],
+  imports: [MongooseModule.forRoot(`${process.env["MONGO_URL"]}`), TasksModule],
 })
 export class AppModule {}
