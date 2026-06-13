cce6a67bf8404f1e19bc49bbe4ea3b32c7710c49
yadira <yadiraco304@gmail.com>
2026-06-13 10:08:41 -0500

    fix: resolve errors

--- back-front/taskapi/src/app.module.ts
+++ back-front/taskapi/src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from "@nestjs/common";
 import { TasksModule } from "./tasks/tasks.module";
 import { MongooseModule } from "@nestjs/mongoose";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 @Module({
   imports: [MongooseModule.forRoot(`${process.env["MONGO_URL"]}`), TasksModule],
